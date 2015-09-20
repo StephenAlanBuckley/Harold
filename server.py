de5edf0584c145ee0c2726f7cb10027adbc09dc8
Stephen Buckley <stephen.alan.buckley@gmail.com>
2015-09-19 21:23:38 -0400

    back to basics

--- server.py
+++ server.py
@@ -4,18 +4,14 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config.from_object(os.environ['APP_SETTINGS'])
 print(os.environ['APP_SETTINGS'])
 Database = SQLAlchemy(app)
 
 @app.route('/')
-def Harold_Homepage(environ, start_response):
-      data = "Hello, World!\n"
-      start_response("200 OK", [
-          ("Content-Type", "text/plain"),
-          ("Content-Length", str(len(data)))
-      ])
-      return iter([data])
+def Harold_Homepage():
+      return "hi"
 
 if __name__ == "__main__":
-      app.run(host='0.0.0.0')
+      app.run()
